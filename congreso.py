598797389febe2127c632f1429caaa99afc5fea3
Jose Enrique Ruiz Navarro <joseenriqueruiznavarro@gmail.com>
2016-09-10 18:20:36 +0200

    move any functions

--- congreso.py
+++ congreso.py
@@ -15,61 +15,6 @@ class Congress(object):
     def searchAll(self,collection=None):
         return [ element for element in self._getCollection(collection).find()]
 
-    def getMember(self,collection= "diputados", name = None):
-        search = self._getCollection(collection).find_one({
-            'nombre': name
-        })
-        try:
-            return search
-        except:
-            return None
-
-    def getGroupfrommember(self, collection= "diputados", name = None):
-        search = self.getMember(collection,name)
-
-        return self.getGroup(name=search['grupo'])
-
-
-    def getGroup(self, collection = "grupos", name = None):
-        #sebusca por acronimo
-        acronimo = None
-        if re.search("la izquierda plural",name,re.IGNORECASE)  \
-                or re.search("GIP",name,re.IGNORECASE) :
-            acronimo = u'GIP'
-        elif re.search("popular",name,re.IGNORECASE) \
-                or re.search("GP",name,re.IGNORECASE):
-            acronimo = u'GP'
-        elif re.search("vasco", name, re.IGNORECASE) \
-                or re.search("EAJ-PNV",name,re.IGNORECASE):
-            acronimo = u'GV (EAJ-PNV)'
-        elif re.search("socialista", name, re.IGNORECASE) \
-                or re.search("GS",name,re.IGNORECASE):
-            acronimo = u'GS'
-        elif re.search("mixto", name, re.IGNORECASE) \
-            or re.search("GMx",name,re.IGNORECASE):
-            acronimo = u'GMx'
-        elif re.search("converg", name, re.IGNORECASE) \
-            or re.search("GC-CiU",name,re.IGNORECASE):
-            acronimo = u'GC-CiU'
-        elif re.search("progreso", name, re.IGNORECASE) \
-                or re.search("GUPyD",name,re.IGNORECASE):
-            acronimo = u'GUPyD'
-        try:
-            search = self._getCollection(collection).find_one({
-            'acronimo': acronimo
-            })
-        except:
-            return None
-
-        return search
-
-    def typeAutor(self, name):
-        if self.getMember(name=name):
-            return "diputado"
-        elif self.getGroup(name=name):
-            return "grupo"
-        else:
-            return "otro"
 
 
     def getInitiative(self, collection="iniciativas", ref = None, tipotexto= None, titulo = None):
